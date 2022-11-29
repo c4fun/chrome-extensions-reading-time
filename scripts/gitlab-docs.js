5f12622a6fcd9f716b333d680c147aaddb3ee932
c4fun <pcfeng502@gmail.com>
2022-11-29 19:57:08 +0800

    added content_scripts for docs.gitlab.com

---
+++ scripts/gitlab-docs.js
@@ -0,0 +1,24 @@
+// GitLab的文档使用了main来标志，搜索HTML里面的<main>标签即可以搜索到
+const article = document.querySelector("main");
+
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (article) {
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const words = text.matchAll(wordMatchRegExp);
+  // matchAll returns an iterator, convert to array to get word count
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+  // Use the same styling as the publish information in an article's header
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} 分钟阅读`;
+
+  // 将badge插入到heading或者date后（我们这里其实插的是heading，因为只有一个h1标签）
+  // Support for API reference docs
+  const heading = article.querySelector("h1");
+  // Support for article docs with date
+  const date = article.querySelector("time")?.parentNode;
+
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+}
